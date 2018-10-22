476b528243ec2b48d59aa7135dedad52ec6ab724
Steve Clewer <steve.clewer@amido.com>
2018-10-22 11:19:17 +0100

    SIDM-1910: Define vault secret variable names in config

--- src/main/java/com/amido/healthchecker/HealthcheckerApplication.java
+++ src/main/java/com/amido/healthchecker/HealthcheckerApplication.java
@@ -2,6 +2,7 @@ package com.amido.healthchecker;
 
 import com.amido.healthchecker.azure.VaultService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -10,14 +11,23 @@ import javax.annotation.PostConstruct;
 @SpringBootApplication
 public class HealthcheckerApplication {
 
+    @Value("${am.secret.name.password}")
+    private String amSecretNamePassword;
+
+    @Value("${am.secret.name.smoke.test.user.username}")
+    private String amSecretNameSmokeTestUserUsername;
+
+    @Value("${am.secret.name.smoke.test.user.password}")
+    private String amSecretNameSmokeTestUserPassword;
+
     @Autowired
     VaultService vaultService;
 
     @PostConstruct
     public void init() {
-        vaultService.loadSecret("AM_PASSWORD", "am-password");
-        vaultService.loadSecret("SMOKE_TEST_USER_USERNAME", "smoke-test-user-username");
-        vaultService.loadSecret("SMOKE_TEST_USER_PASSWORD", "smoke-test-user-password");
+        vaultService.loadSecret("AM_PASSWORD", amSecretNamePassword);
+        vaultService.loadSecret("SMOKE_TEST_USER_USERNAME", amSecretNameSmokeTestUserUsername);
+        vaultService.loadSecret("SMOKE_TEST_USER_PASSWORD", amSecretNameSmokeTestUserPassword);
     }
 
     public static void main(String[] args) {
