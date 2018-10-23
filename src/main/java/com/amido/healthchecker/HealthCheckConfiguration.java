6896ac4289b97d7bb5d16fcc63ba1478ba2a3715
Steve Clewer <steve.clewer@amido.com>
2018-10-23 11:47:21 +0100

    SIDM-1910: Added default timeouts to Feign clients

--- src/main/java/com/amido/healthchecker/HealthCheckConfiguration.java
+++ src/main/java/com/amido/healthchecker/HealthCheckConfiguration.java
@@ -3,6 +3,7 @@ package com.amido.healthchecker;
 import com.amido.healthchecker.health.am.AMFeignClient;
 import com.amido.healthchecker.health.idm.IDMFeignClient;
 import feign.Feign;
+import feign.Request;
 import feign.form.FormEncoder;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
@@ -17,13 +18,24 @@ public class HealthCheckConfiguration {
     @Value("${idm.uri}")
     private String idmUri;
 
+    @Value("${feign.connect.timeout.millis:5000}")
+    private int connectTimeoutMillis;
+
+    @Value("${feign.read.timeout.millis:60000}")
+    private int readTimeoutMillis;
+
     @Bean
     AMFeignClient amFeignClient() {
-        return Feign.builder().encoder(new FormEncoder()).target(AMFeignClient.class, amUri);
+        return Feign.builder().encoder(new FormEncoder()).options(timeoutOptions()).target(AMFeignClient.class, amUri);
     }
 
     @Bean
     IDMFeignClient idmFeignClient() {
-        return Feign.builder().encoder(new FormEncoder()).target(IDMFeignClient.class, idmUri);
+        return Feign.builder().encoder(new FormEncoder()).options(timeoutOptions()).target(IDMFeignClient.class, idmUri);
+    }
+
+    @Bean
+    public Request.Options timeoutOptions() {
+        return new Request.Options(connectTimeoutMillis, readTimeoutMillis);
     }
 }
