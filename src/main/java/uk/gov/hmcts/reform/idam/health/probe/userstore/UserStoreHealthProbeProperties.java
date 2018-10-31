2ac94761acdbd92769071aaed82b880eccce7ad3
Kremi Nenkova <kremi.nenkova@gmail.com>
2018-10-31 23:29:32 +0000

    Configuration properties for userstore.

---
+++ src/main/java/uk/gov/hmcts/reform/idam/health/probe/userstore/UserStoreHealthProbeProperties.java
@@ -0,0 +1,23 @@
+package uk.gov.hmcts.reform.idam.health.probe.userstore;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Component
+@ConfigurationProperties("userstore.healthprobe")
+@Getter
+@Setter
+public class UserStoreHealthProbeProperties {
+
+    @Getter
+    @Setter
+    static class Authentication {
+
+        private Long freshnessInterval;
+        private Long checkInterval;
+    }
+
+    private Authentication authentication;
+}
