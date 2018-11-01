d09c577bd4adb49152dc2b3a2f9abd1f42dd2aad
Kremi Nenkova <kremi.nenkova@gmail.com>
2018-11-01 17:38:19 +0000

    Default server port. OptimisticHealthProbe fix.

--- src/main/java/uk/gov/hmcts/reform/idam/health/probe/OptimisticHealthProbe.java
+++ src/main/java/uk/gov/hmcts/reform/idam/health/probe/OptimisticHealthProbe.java
@@ -10,7 +10,7 @@ import org.springframework.stereotype.Component;
 public class OptimisticHealthProbe implements HealthProbe {
 
     @Override
-    public boolean isOkay() {
+    public boolean probe() {
         return true;
     }
 }
