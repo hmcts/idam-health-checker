71e43129d9c1818caa6a957da607c9a7d08126cf
jburke-amido <james.burke@amido.com>
2018-11-01 10:53:51 +0000

    use map response for IDM

--- src/main/java/uk/gov/hmcts/reform/idam/health/probe/idm/IdmPingHealthStatus.java
+++ src/main/java/uk/gov/hmcts/reform/idam/health/probe/idm/IdmPingHealthStatus.java
@@ -1,20 +1,22 @@
 package uk.gov.hmcts.reform.idam.health.probe.idm;
 
-import feign.Response;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.collections4.MapUtils;
 import org.springframework.context.annotation.Profile;
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 import uk.gov.hmcts.reform.idam.health.probe.HealthStatus;
 import uk.gov.hmcts.reform.idam.health.probe.Status;
 
 import java.util.Base64;
+import java.util.Map;
 
 @Component
 @Profile("idm")
 @Slf4j
 public class IdmPingHealthStatus implements HealthStatus {
 
+    private static final String STATE = "state";
+    private static final String IDM_ACTIVE = "ACTIVE_READY";
     private static String ANONYMOUS_USER = "anonymous";
     private static String ANONYMOUS_PASSWORD = "anonymous";
 
@@ -29,8 +31,9 @@ public class IdmPingHealthStatus implements HealthStatus {
     @Override
     public Status determineStatus() {
         try {
-            Response pingResponse = idmProvider.ping(authorization);
-            if (pingResponse.status() == HttpStatus.OK.value()) {
+            Map<String, String> response = idmProvider.ping(authorization);
+            if (IDM_ACTIVE.equals(MapUtils.getString(response, STATE))) {
+                log.info("IDM Ping OK");
                 return Status.UP;
             }
         } catch (Exception e) {
--- src/main/java/uk/gov/hmcts/reform/idam/health/probe/idm/IdmProvider.java
+++ src/main/java/uk/gov/hmcts/reform/idam/health/probe/idm/IdmProvider.java
@@ -1,10 +1,11 @@
 package uk.gov.hmcts.reform.idam.health.probe.idm;
 
-import feign.Response;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.context.annotation.Profile;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
+
+import java.util.Map;
 
 @FeignClient(name = "IdmProvider", url = "${idm.root}")
 @Profile("idm")
@@ -13,5 +14,5 @@ public interface IdmProvider {
     @GetMapping(
             value = "/info/ping"
     )
-    Response ping(@RequestHeader("Authorization") String basicAuth);
+    Map<String, String> ping(@RequestHeader("Authorization") String basicAuth);
 }
