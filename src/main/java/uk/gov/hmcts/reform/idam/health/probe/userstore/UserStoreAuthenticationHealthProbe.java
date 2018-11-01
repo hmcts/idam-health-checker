e2e3dc8fa7597248f1ed35d246bf91a2fb10c279
Kremi Nenkova <kremi.nenkova@gmail.com>
2018-11-01 15:19:39 +0000

    UserStore rework.

--- src/main/java/uk/gov/hmcts/reform/idam/health/probe/userstore/UserStoreAuthenticationHealthProbe.java
+++ src/main/java/uk/gov/hmcts/reform/idam/health/probe/userstore/UserStoreAuthenticationHealthProbe.java
@@ -4,14 +4,13 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Profile;
 import org.springframework.ldap.core.LdapTemplate;
 import org.springframework.stereotype.Component;
-import uk.gov.hmcts.reform.idam.health.probe.HealthStatus;
-import uk.gov.hmcts.reform.idam.health.probe.Status;
+import uk.gov.hmcts.reform.idam.health.probe.HealthProbe;
 import uk.gov.hmcts.reform.idam.health.probe.env.ProbeUserProperties;
 
 @Component
 @Profile("userstore")
 @Slf4j
-public class UserStoreAuthenticationHealthStatus implements HealthStatus {
+public class UserStoreAuthenticationHealthProbe implements HealthProbe {
 
     private static final String LDAP_PARTITION_SUFFIX = "dc=reform,dc=hmcts,dc=net";
     private static final String LDAP_USER_FILTER_TEMPLATE = "(uid=%s)";
@@ -21,7 +20,9 @@ public class UserStoreAuthenticationHealthStatus implements HealthStatus {
     private final String ldapUserFilter;
     private final String ldapUserPassword;
 
-    public UserStoreAuthenticationHealthStatus(LdapTemplate ldapTemplate, ProbeUserProperties testUserProperties) {
+    public UserStoreAuthenticationHealthProbe(
+            LdapTemplate ldapTemplate,
+            ProbeUserProperties testUserProperties) {
         this.ldapTemplate = ldapTemplate;
 
         this.ldapUserFilter = String.format(LDAP_USER_FILTER_TEMPLATE, testUserProperties.getUsername());
@@ -29,16 +30,11 @@ public class UserStoreAuthenticationHealthStatus implements HealthStatus {
     }
 
     @Override
-    public Status determineStatus() {
-        try {
-            boolean isAuthenticationSuccessful = ldapTemplate.authenticate(
-                    LDAP_PARTITION_SUFFIX,
-                    ldapUserFilter,
-                    ldapUserPassword);
-            return isAuthenticationSuccessful ? Status.UP : Status.DOWN;
-        } catch (Exception e) {
-            log.error("USERSTORE: " + e.getMessage());
-            return Status.DOWN;
-        }
+    public boolean probe() {
+        boolean isAuthenticationSuccessful = ldapTemplate.authenticate(
+                LDAP_PARTITION_SUFFIX,
+                ldapUserFilter,
+                ldapUserPassword);
+        return isAuthenticationSuccessful;
     }
 }
---
+++ src/main/java/uk/gov/hmcts/reform/idam/health/probe/userstore/UserStoreHealthProbeConfiguration.java
@@ -0,0 +1,24 @@
+package uk.gov.hmcts.reform.idam.health.probe.userstore;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import uk.gov.hmcts.reform.idam.health.probe.ScheduledHealthProbe;
+
+@Configuration
+@Profile("userstore")
+public class UserStoreHealthProbeConfiguration {
+
+    @Autowired
+    private UserStoreHealthProbeProperties userStoreHealthProbeProperties;
+
+    @Bean
+    public ScheduledHealthProbe userStoreAuthenticationHealthProbe(
+            UserStoreAuthenticationHealthProbe userStoreAuthenticationHealthProbe) {
+        return new ScheduledHealthProbe(
+                userStoreAuthenticationHealthProbe,
+                userStoreHealthProbeProperties.getAuthentication().getFreshnessInterval(),
+                userStoreHealthProbeProperties.getAuthentication().getCheckInterval());
+    }
+}
