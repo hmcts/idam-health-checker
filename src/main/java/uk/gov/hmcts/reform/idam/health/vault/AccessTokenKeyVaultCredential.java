61688618b959cf36c96e2210d3a99777b9d447bd
Steve Clewer <steve.clewer@amido.com>
2018-12-18 16:49:31 +0000

    Factored out TokenResponseHandler as a singleton

--- src/main/java/uk/gov/hmcts/reform/idam/health/vault/AccessTokenKeyVaultCredential.java
+++ src/main/java/uk/gov/hmcts/reform/idam/health/vault/AccessTokenKeyVaultCredential.java
@@ -1,28 +1,17 @@
 package uk.gov.hmcts.reform.idam.health.vault;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.node.ObjectNode;
 import com.microsoft.azure.AzureEnvironment;
 import com.microsoft.azure.credentials.AzureTokenCredentials;
-import org.apache.http.HttpEntity;
-import org.apache.http.HttpResponse;
-import org.apache.http.HttpStatus;
-import org.apache.http.StatusLine;
 import org.apache.http.client.HttpClient;
-import org.apache.http.client.HttpResponseException;
-import org.apache.http.client.ResponseHandler;
 import org.apache.http.client.methods.HttpUriRequest;
 import org.apache.http.client.methods.RequestBuilder;
 import org.apache.http.impl.client.HttpClientBuilder;
-import org.apache.http.util.EntityUtils;
 
 import java.io.IOException;
-import java.nio.charset.StandardCharsets;
 
 public class AccessTokenKeyVaultCredential extends AzureTokenCredentials {
 
     private final static String METADATA_HEADER = "Metadata";
-    private final static String ACCESS_TOKEN_KEY = "access_token";
 
     private final String tokenEndpoint;
 
@@ -39,30 +28,6 @@ public class AccessTokenKeyVaultCredential extends AzureTokenCredentials {
                 .setHeader(METADATA_HEADER, Boolean.TRUE.toString())
                 .build();
 
-        return client.execute(request, new TokenResponseHandler());
+        return client.execute(request, TokenResponseHandler.getInstance());
     }
-
-    protected class TokenResponseHandler implements ResponseHandler<String> {
-
-        ObjectMapper mapper = new ObjectMapper();
-
-        @Override
-        public String handleResponse(HttpResponse response) throws IOException {
-            StatusLine statusLine = response.getStatusLine();
-            if (statusLine.getStatusCode() != HttpStatus.SC_OK) {
-                throw new HttpResponseException(statusLine.getStatusCode(), statusLine.getReasonPhrase());
-            }
-
-            HttpEntity entity = response.getEntity();
-            String json = EntityUtils.toString(entity, StandardCharsets.UTF_8);
-
-            ObjectNode node = mapper.readValue(json, ObjectNode.class);
-            if (node.has(ACCESS_TOKEN_KEY)) {
-                return node.get(ACCESS_TOKEN_KEY).asText();
-            }
-
-            throw new HttpResponseException(statusLine.getStatusCode(), "No access_token parameter present in response");
-        }
-    }
-
 }
